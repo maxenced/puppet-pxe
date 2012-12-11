77592b4fbd1c84e9e58d5832bcbed560e67067fa
root <root@debian.bonfire.grid5000.fr>
2012-12-11 11:38:34 +0100

    update menu organization

--- manifests/setup/menu.pp
+++ manifests/setup/menu.pp
@@ -21,18 +21,18 @@ class pxe::setup::menu {
         template  => 'pxe/menu_default.erb';
     }
 
-#    pxe::menu::entry { 'Installations':
-#        file    => 'default',
-#        append  => 'pxelinux.cfg/menu_install',
-#    }
-
-#    pxe::menu { 'Operating System ($arch)':
-#        file  => 'menu_install',
-#    }
+    pxe::menu::entry { 'Installations':
+        file    => 'default',
+        append  => 'pxelinux.cfg/menu_install',
+    }
 
     pxe::menu::entry { 'Debian':
         file    => 'menu_install',
         append  => 'pxelinux.cfg/os_debian',
+    }
+
+    pxe::menu { 'Debian versions':
+	file    => 'os_debian'
     }
 
     pxe::menu::entry { 'Debian 6 squeeze amd64 Installation':
